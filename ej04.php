39acde5c21904c0d954c3dfb84b31efb10c33a93
Ángeles <abuzon+git@posteo.es>
2025-11-26 09:38:09 +0100

    Termino ej4

--- ej04.php
+++ ej04.php
@@ -8,13 +8,50 @@
  * • Calcular el salario anual automáticamente
  */
 
-include "/clases/Empleado.php";
+// CLASES
+class Empleado {
+    public string $nombre {
+        get => strtoupper($this->nombre); 
+    }
 
+    public float $salario_anual = 0; // 0 por defecto
+
+    public float $salario = 0 {
+        set(float $cantidad) {
+            if ($cantidad < 0) {
+                throw new Exception("El salario no puede ser negativo");
+            }
+
+            $this->salario = $cantidad;
+            $this->salario_anual = $this->salario * 12;
+        }
+    }
+
+    public function __construct($nombre="Desconocido") {
+        // El nombre es "Desconocido" si no se especifica en el constructor
+        $this->nombre = $nombre;
+    }
+}
+
+// EJECUCIÓN
 try {
     $primer_empleado = new Empleado();
-} catch ($e) {
+    $primer_empleado->salario = 1300;
+    echo "Salario de $primer_empleado->nombre: $primer_empleado->salario EUR ($primer_empleado->salario_anual EUR al año)\n";
 
+    $segundo_empleado = new Empleado("Juanita");
+    $segundo_empleado->salario = 2000;
+    echo "Salario de $segundo_empleado->nombre: $segundo_empleado->salario EUR ($segundo_empleado->salario_anual EUR al año)\n";
+
+    // Lo siguiente dará error
+    $tercer_empleado = new Empleado("Atanasio");
+    $tercer_empleado->salario = -5;
+    echo "Salario de $tercer_empleado->nombre: $tercer_empleado->salario EUR ($tercer_empleado->salario_anual EUR al año)\n";
+
+} catch (Exception $e) {
+    echo "Error: " . $e->getMessage() . "\n";
 }
+
 
 
 ?>
